50499ed46c5f8e1ff9525a47ef138b9965d63813
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-09-02 14:38:43 -0600

    whoAreYou Created

---
+++ whoAreYou/main.py
@@ -0,0 +1,6 @@
+#Jonas Fairchild Who are you?
+
+name = input("Hi! What's your name?: ")
+color = input("What's your favorite color?: ")
+age = input("How old are you?: ")
+print("So, your name is", name + ", you like", color + ", and you're", age + " years old. Cool!")
