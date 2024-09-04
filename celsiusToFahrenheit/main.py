88399c632c612852fa68d6b97f3dac890dde16ea
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-09-04 10:46:52 -0600

    Celsius to Fahrenheit

---
+++ celsiusToFahrenheit/main.py
@@ -0,0 +1,6 @@
+#Jonas Fairchild Celsius to Fahrenheit
+
+print("This program will convert Celsius to Fahrenheit.")
+celsius = int(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius*(9/5))+32
+print(f"When it is {celsius} degrees Celsius, it is {fahrenheit} degrees Fahrenheit.")
