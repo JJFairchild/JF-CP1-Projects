c9f55733434404557b647de865cacad11596eb47
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-09-24 18:10:23 -0600

    Pig Latin Final Update

--- pigLatinConverter/main.py
+++ pigLatinConverter/main.py
@@ -1,14 +1,12 @@
-#Jonas Fairchild Pig Latin Converter
 
 word = input("Type the word you want to convert to Pig Latin.: ")
 
 def isVowel(w, x):
-    w[x] == 'a' or w[x] == 'e' or w[x] == 'i' or w[x] == 'o' or w[x] == 'u'
-    
+    return w[x] == 'a' or w[x] == 'e' or w[x] == 'i' or w[x] == 'o' or w[x] == 'u'
+
+w = ''
 if isVowel(word, 0):
     w = 'w'
-else:
-    w = ''
 
 while isVowel(word, 0) == False:
     word = word[1:] + word[0]
