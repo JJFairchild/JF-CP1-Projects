a78b987745fb4be1e4b6fb3b9af21041e2c10031
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-09-25 10:48:54 -0600

    Secret Cipher

--- secretCypher/main.py
+++ secretCypher/main.py
@@ -1,8 +1,67 @@
-[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-txt = input("Type the text you want to convert to cypher.: ")
-shift = input("Type the shift you want to shift the letters by.: ")
-newText = ""
-for i in txt:
-    newText += txt[i]
+#Jonas Fairchild, Secret Cipher
 
-print(newText)
+txt = input("Type the text you want to convert to cypher. (shift = 2): ")
+
+def map(l):
+    if l == 'a':
+        l = 'c'
+    elif l == 'b':
+        l = 'd'
+    elif l == 'c':
+        l = 'e'
+    elif l == 'd':
+        l = 'f'
+    elif l == 'e':
+        l = 'g'
+    elif l == 'f':
+        l = 'h'
+    elif l == 'g':
+        l = 'i'
+    elif l == 'h':
+        l = 'j'
+    elif l == 'i':
+        l = 'k'
+    elif l == 'j':
+        l = 'l'
+    elif l == 'k':
+        l = 'm'
+    elif l == 'l':
+        l = 'n'
+    elif l == 'm':
+        l = 'o'
+    elif l == 'n':
+        l = 'p'
+    elif l == 'o':
+        l = 'q'
+    elif l == 'p':
+        l = 'r'
+    elif l == 'q':
+        l = 's'
+    elif l == 'r':
+        l = 't'
+    elif l == 's':
+        l = 'u'
+    elif l == 't':
+        l = 'v'
+    elif l == 'u':
+        l = 'w'
+    elif l == 'v':
+        l = 'x'
+    elif l == 'w':
+        l = 'y'
+    elif l == 'x':
+        l = 'z'
+    elif l == 'y':
+        l = 'a'
+    elif l == 'z':
+        l = 'b'
+    return l
+
+newTxt = ''
+num = 0
+while not len(txt) == len(newTxt):
+    newTxt = newTxt + map(txt[num])
+    num += 1
+
+print("Original:" + txt)
+print("New:" + newTxt)
