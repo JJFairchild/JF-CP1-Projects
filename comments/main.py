658a5e931de104f84a198112f231f46608fed092
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-10-01 10:37:11 -0600

    Comments

---
+++ comments/main.py
@@ -0,0 +1,9 @@
+#Jonas Fairchild, comment practicing
+
+#This code asks for a user's name, then greets them.
+
+
+
+
+name = input("What is your name?: ")#Requests the user to type their name, and stores it in a variable 'name'
+print("Hello," ,name)#Prints 'hello' to the name that the user typed.
