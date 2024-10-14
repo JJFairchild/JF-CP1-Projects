2f1cd5efa9f25a6c21a956236ec4ac41c2506c87
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-10-14 10:45:57 -0600

    lists

---
+++ lists/main.py
@@ -0,0 +1,6 @@
+#Jonas Fairchild, list of classes
+
+classes = ['Math', 'Computer Science', 'Biology', 'Lunch', 'Advisory', 'Geography', 'English', 'Drawing']
+classNumber = int(input("Which period would you like to know about?: "))
+classNumber = int(input("Which period would you like to know about?: "))
+print(f"During {classNumber}th period, I have {classes[classNumber - 1]}.")
