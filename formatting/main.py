5a8ae1180e3d5442be51434cb642728902b6fdc8
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-10-15 10:39:15 -0600

    formatting fix

--- formatting/main.py
+++ formatting/main.py
@@ -2,7 +2,7 @@
 
 num = int(input("What number would you like to format?: "))
 print("This number as an integer with commas is: {:,}".format(num))
-print(f"This number as a float with 4 decimal places is: {num/10000}") #Note: This only works if your number is at least 4 digits long and doesn't end in a 0.
+print("This number as a float with 4 decimal places is: {:.4f}".format(num))
 print("This number as a percentage is: {:%}".format(num))
-print(f"This number as an amount in dollars is: {"$" + str(num/100)}")
+print("This number as an amount in dollars is: ${:.2f}".format(num))
 
