fe5da6abe3938404322c100911e7f7109ad44943
JJFairchild <jonas.fairchild@ucas-edu.net>
2024-12-17 12:23:18 -0700

    Finals 2nd-last update

--- finalProject/main.py
+++ finalProject/main.py
@@ -2,12 +2,13 @@
 
 """
 TO DO:
-- Make and test combat system with a miniboss
+- Add picking up items
 - Make the map
 """
 
 import random
 import time
+import copy
 
 try: #Checks if the keyboard is installed and does not run the rest of the program if not.
     import keyboard
@@ -25,8 +26,8 @@ if moveError:
 if moveError:
     while True:
         try:
-            screenSize = int(int(input("What do you want your screen size to be? (range from 10-20): ")) / 2)
-            while screenSize not in range(5, 11):
+            screenSize = int(int(input("What do you want your screen size to be? (Higher = buggier): ")) / 2)
+            while screenSize not in range(5, 16):
                 print("Input out of range. Try again.")
                 screenSize = int(int(input("What do you want your screen size to be? (range from 10-20): ")) / 2)
             else:
@@ -42,10 +43,9 @@ if moveError:
 Use WASD to move. Space does not do anything, just press W.
 Press W while on a ▯ tile to read signs.
 Press E to open the inventory. Press M to open the map.
-All bossfights are optional, but without fighting bosses you won't have the equipment to beat the game.
-Be prepared to enter combat with a smaller creature at random! This will happen about every 4 minutes.
-
-
+All bossfights are optional, but without fighting bosses you won't have the equipment to beat the game. Bosses are marked with an E.
+Be prepared to enter combat with a smaller creature at random! This will happen every few minutes.
+If you see an I on the map, that means an item is there. You should pick it up!
 ''')
             input('Done reading?: ')
             break
@@ -56,9 +56,12 @@ Be prepared to enter combat with a smaller creature at random! This will happen 
             tutorial = input('Would you like a quick tutorial? Y/n: ').lower()
 
 
-blockTypes = ['▮', '▯', 'v', '^', '>', '<', 'C', 'E']
-map = [[-1, -2], 0, [0, -2], 0, [1, -2], 0, [2, -2], 0, [3, -2], 0, [4, -2], 0, [5, -2], 0, [5, -1], 7, 0]
-playerPos = [0, 0]
+blockTypes = ['▮', '▯', 'v', '^', '>', '<', 'C', 'E', 'I']
+
+# Adjusted Tutorial Map (with right-shifted platforms at y = 8)
+map = [[1, 0], 0]
+
+playerPos = [1, 1]
 gravity = 0.25
 velocity = [0, 0]
 mapBoard = '''
@@ -81,44 +84,44 @@ mapBoard = '''
 enemies = [
     # Relatively Easy to Defeat
     ['Shrublike Creature', 25, [['attack', 'Vine Whip', 5], ['defend', 'Root Shield', 5], ['heal', 'Photosynthesize', 8]], 'Small Healing Potion'],
-    ['Red-Eyed Rabbit', 30, [['attack', 'Frantic Bite', 8], ['attack', 'Scratch', 5], ['defend', 'Quick Dodge', 4]], 'Wooden Shield'],
+    ['Red-Eyed Rabbit', 30, [['attack', 'Frantic Bite', 8], ['attack', 'Scratch', 5], ['defend', 'Quick Dodge', 4]], 'Small Healing Potion'],
     
     # Slightly Harder
-    ['Shark', 50, [['attack', 'Bite', 12], ['attack', 'Tail Whip', 7], ['defend', 'Water Veil', 8]], 'Medium Healing Potion'],
-    ['Octopus', 55, [['attack', 'Ink Spray', 8], ['attack', 'Tentacle Slap', 10], ['defend', 'Ink Defense', 6]], 'Medium Healing Potion'],
-    ['Wolf', 60, [['attack', 'Bite', 10], ['attack', 'Howl', 12], ['defend', 'Dodge', 5]], 'Iron Shield'],
-    ['Skinwalker', 65, [['attack', 'Shape Strike', 12], ['attack', 'Terrifying Form', 10], ['heal', 'Absorb Essence', 12]], 'Iron Sword'],
+    ['Shark', 50, [['attack', 'Bite', 12], ['attack', 'Tail Whip', 7], ['defend', 'Water Veil', 8]], 'Small Healing Potion'],
+    ['Octopus', 55, [['attack', 'Ink Spray', 8], ['attack', 'Tentacle Slap', 10], ['defend', 'Ink Defense', 6]], 'Small Healing Potion'],
+    ['Wolf', 60, [['attack', 'Bite', 10], ['attack', 'Howl', 12], ['defend', 'Dodge', 5]], 'Small Healing Potion'],
+    ['Skinwalker', 65, [['attack', 'Shape Strike', 12], ['attack', 'Terrifying Form', 10], ['heal', 'Absorb Essence', 12]], 'Small Healing Potion'],
     
     # Moderately Hard
-    ['Sentient Cactus', 70, [['attack', 'Spike Shoot', 10], ['defend', 'Hardened Spikes', 10], ['heal', 'Absorb Sunlight', 10]]],
-    ['Sandworm', 75, [['attack', 'Burrow Attack', 15], ['defend', 'Sand Veil', 8], ['attack', 'Devour', 20]]],
-    ['Waterbender', 80, [['attack', 'Water Blast', 12], ['attack', 'Tidal Strike', 15], ['defend', 'Water Wall', 12], ['heal', 'Aqua Healing', 15]]],
-    ['Rock Hurler', 85, [['attack', 'Stone Throw', 15], ['defend', 'Rock Barrier', 12], ['attack', 'Boulder Smash', 18]]],
+    ['Sentient Cactus', 70, [['attack', 'Spike Shoot', 10], ['defend', 'Hardened Spikes', 10], ['heal', 'Absorb Sunlight', 10]], 'Medium Healing Potion'],
+    ['Sandworm', 75, [['attack', 'Burrow Attack', 15], ['defend', 'Sand Veil', 8], ['attack', 'Devour', 20]], 'Medium Healing Potion'],
+    ['Waterbender', 80, [['attack', 'Water Blast', 12], ['attack', 'Tidal Strike', 15], ['defend', 'Water Wall', 12], ['heal', 'Aqua Healing', 15]], 'Medium Healing Potion'],
+    ['Rock Hurler', 85, [['attack', 'Stone Throw', 15], ['defend', 'Rock Barrier', 12], ['attack', 'Boulder Smash', 18]], 'Medium Healing Potion'],
     
     # Even Harder
-    ['Massive Frog', 90, [['attack', 'Tongue Lash', 12], ['attack', 'Body Slam', 18], ['defend', 'Swamp Dodge', 10]]],
-    ['Mudsucker', 95, [['attack', 'Mud Slap', 14], ['defend', 'Mud Barrier', 12], ['heal', 'Regenerate Slime', 15]]],
+    ['Massive Frog', 90, [['attack', 'Tongue Lash', 12], ['attack', 'Body Slam', 18], ['defend', 'Swamp Dodge', 10]], 'Large Healing Potion'],
+    ['Mudsucker', 95, [['attack', 'Mud Slap', 14], ['defend', 'Mud Barrier', 12], ['heal', 'Regenerate Slime', 15]], 'Large Healing Potion'],
     
     # Very Hard
-    ['Falcon', 100, [['attack', 'Dive Bomb', 15], ['attack', 'Talon Strike', 12], ['defend', 'Soar Away', 10]]],
-    ['Golem', 110, [['attack', 'Rock Smash', 18], ['defend', 'Stone Armor', 15], ['heal', 'Absorb Earth', 20]]],
+    ['Falcon', 100, [['attack', 'Dive Bomb', 15], ['attack', 'Talon Strike', 12], ['defend', 'Soar Away', 10]], 'Large Healing Potion'],
+    ['Golem', 110, [['attack', 'Rock Smash', 18], ['defend', 'Stone Armor', 15], ['heal', 'Absorb Earth', 20]], 'Large Healing Potion'],
     
     # Hardest Non-Boss Enemies
-    ['Lava Bird', 120, [['attack', 'Fiery Peck', 18], ['attack', 'Wing Flare', 22], ['defend', 'Flame Guard', 15]]],
-    ['Magma Slime', 125, [['attack', 'Lava Splash', 20], ['attack', 'Heat Pulse', 25], ['defend', 'Molten Shell', 18], ['heal', 'Molten Regeneration', 18]]],
+    ['Lava Bird', 120, [['attack', 'Fiery Peck', 18], ['attack', 'Wing Flare', 22], ['defend', 'Flame Guard', 15]], 'Elixr of Life'],
+    ['Magma Slime', 125, [['attack', 'Lava Splash', 20], ['attack', 'Heat Pulse', 25], ['defend', 'Molten Shell', 18], ['heal', 'Molten Regeneration', 18]], 'Elixr of Life'],
     
     # Minibosses
-    ['Grassworm', 120, [['attack', 'Vine Crush', 20], ['defend', 'Leaf Shield', 15], ['heal', 'Regrow', 10]]],
-    ['The Silent One', 130, [['attack', 'Shadow Claw', 22], ['attack', 'Silence Strike', 25], ['defend', 'Shadow Fade', 12]]],
-    ['The Sandmaster', 140, [['attack', 'Sandstorm', 28], ['defend', 'Sand Cloak', 15], ['heal', 'Desert Absorption', 25]]],
-    ['Skrum', 150, [['attack', 'Tentacle Slam', 30], ['attack', 'Ink Burst', 25], ['defend', 'Abyssal Dodge', 18]]],
-    ['Gust', 135, [['attack', 'Hurricane Strike', 24], ['attack', 'Wind Blade', 20], ['defend', 'Gale Shield', 15], ['heal', 'Whirlwind Regeneration', 20]]],
+    ['Grassworm', 120, [['attack', 'Vine Crush', 20], ['defend', 'Leaf Shield', 15], ['heal', 'Regrow', 15]], 'Wooden Shield'],
+    ['The Silent One', 130, [['attack', 'Shadow Claw', 22], ['attack', 'Silence Strike', 25], ['defend', 'Shadow Fade', 12]], 'Iron Shield'],
+    ['The Sandmaster', 140, [['attack', 'Sandstorm', 28], ['defend', 'Sand Cloak', 15], ['heal', 'Desert Absorption', 25]], 'Steel Shield'],
+    ['Skrum', 150, [['attack', 'Tentacle Slam', 30], ['attack', 'Ink Burst', 25], ['defend', 'Abyssal Dodge', 18]], 'Platinum Shield'],
+    ['Gust', 135, [['attack', 'Hurricane Strike', 24], ['attack', 'Wind Blade', 20], ['defend', 'Gale Shield', 15], ['heal', 'Whirlwind Regeneration', 20]], 'Platinum Shield'],
     ['Vulcan', 200, [['attack', 'Molten Smash', 35], ['attack', 'Eruption', 40], ['defend', 'Lava Armor', 20], ['heal', 'Core Heat Recovery', 30]]]
 ]
 
 health = 100 
 coins = 0
-inventory = ['Stone Sword']
+inventory = []
 
 def area(x1, y1, x2, y2): #Defines a function which takes two coordinate points and selects every point between them.
     areaList = []
@@ -128,6 +131,8 @@ def area(x1, y1, x2, y2): #Defines a function which takes two coordinate points 
     return areaList
 
 def displayScreen():
+    for x in range(100):
+        print()
     row = []
     prevY = playerPos[1] + 5
     for space in area(playerPos[0] - screenSize, playerPos[1] - screenSize, playerPos[0] + screenSize, playerPos[1] + screenSize):
@@ -145,6 +150,7 @@ def displayScreen():
         prevY = space[1]
 
 def collision(): #Checks for objects around the player.
+    global coins
     coll_returns = []
     for space in area(playerPos[0] - 1, playerPos[1] - 1, playerPos[0] + 1, playerPos[1] + 1):
         if space in map:
@@ -159,13 +165,26 @@ def collision(): #Checks for objects around the player.
                     coll_returns.append('s')
             if blockTypes[map[map.index(space) + 1]] in ['v', '^', '>', '<'] and space == playerPos: #Returns a death if a spike overlaps the player.
                 coll_returns.append('dead')
-            if blockTypes[map[map.index(space) + 1]] == 'C' and space == playerPos: #If a coin is detected, remove it from the board and add it to the score.
-                del map[map.index(space) + 1]
-                map.remove(map[space])
-                coll_returns.append('coin')
             if blockTypes[map[map.index(space) + 1]] == 'E' and space == playerPos:
                 coll_returns.append('enemy')
                 coll_returns.append(map[map.index(space) + 2])
+            if blockTypes[map[map.index(space) + 1]] == '▯' and space == playerPos:
+                coll_returns.append('sign')
+                coll_returns.append(map[map.index(space) + 2])
+            if blockTypes[map[map.index(space) + 1]] == 'I' and space == playerPos: #If a coin is detected, remove it from the board and add it to the score.
+                inventory.append(map[map.index(space) + 2])
+                print()
+                print(f"Congratulations, you found a {map[map.index(space) + 2]}!")
+                del map[map.index(space) + 1]
+                del map[map.index(space)]
+                time.sleep(2)
+            if blockTypes[map[map.index(space) + 1]] == 'C' and space == playerPos: #If a coin is detected, remove it from the board and add it to the score.
+                coins += 1
+                print()
+                print("Congratulations, you found a coin!")
+                del map[map.index(space) + 1]
+                del map[map.index(space)]
+                time.sleep(2)
     return coll_returns
 
 if moveError:
@@ -178,10 +197,14 @@ if moveError:
             if key in collision():
                 del keysPressed[keysPressed.index(key)]
             
-        if 'w' in keysPressed and 's' in collision():
+        if 'w' in keysPressed and 's' in collision() and 'sign' not in collision():
             velocity[1] = 2
-        elif 'w' in collision() and velocity[1] > 0:
+        elif 'w' in collision() and velocity[1] > 0 and 'sign' not in collision():
             velocity[1] = 0
+        elif 'w' in keysPressed and 'sign' in collision():
+            print()
+            print(collision()[collision().index('sign') + 1])
+            input("Done reading?: ")
         if 'a' in keysPressed and velocity[0] > -1:
             velocity[0] -= 0.25
         elif 'a' in collision() and velocity[0] < 0:
@@ -204,14 +227,16 @@ if moveError:
         return [playerPos[0] + int(velocity[0]), playerPos[1] + int(velocity[1])]
 
     def playMap(): #A set of other functions collected to do everything needed to run the platformer part.
+        global coins
         if keyboard.is_pressed('e') and not keyboard.is_pressed('m'):
             for item in inventory:
                 print(item, end=', ')
+            input("Done reading?: ")
         elif keyboard.is_pressed('m') and not keyboard.is_pressed('e'):
             print(mapBoard)
+            input("Done reading?: ")
         else:
             displayScreen()
-            print(playerPos)
         time.sleep(0.25)
         
 def choices(): #Every item in the game that can be both picked up and used. Gives the action you can do with the item, and what that action does.
@@ -248,9 +273,17 @@ def choices(): #Every item in the game that can be both picked up and used. Give
 
     return choiceReturns
 
+def erase(enemy):
+    global map
+    for space in area(enemy[0] - 10, enemy[1] - 10, enemy[0] + 10, enemy[1] + 10):
+        if space in map:
+            if blockTypes[map[map.index(space) + 1]] == 'E':
+                del map[map.index(space) + 1]
+                del map[map.index(space)]
+
 def combat(enemy): #The combat system for enemies. Takes a specific enemy, lets the user make a choice, does a random action for the enemy, and repeats until someone dies.
     global health
-    tempEnemy = enemies[enemy]
+    tempEnemy = copy.deepcopy(enemies[enemy])
     userImmunity = 0
     enemyImmunity = 0
     print(f"Oh no, you encountered a {tempEnemy[0]}!")
@@ -328,6 +361,7 @@ def combat(enemy): #The combat system for enemies. Takes a specific enemy, lets 
         for item in choices():
             if item[0] == userAttack:
                 print(item[2])
+                print()
                 if item[1] == 'attack':
                     userImmunity = 0
                     tempEnemy[1] -= (abs(item[3] - enemyImmunity)) #Subtracts the enemy’s health by the damage of the user’s attack.
@@ -342,8 +376,11 @@ def combat(enemy): #The combat system for enemies. Takes a specific enemy, lets 
         
         if tempEnemy[1] <= 0: #If enemy is dead
             print(f'Congratulations, you defeated the {tempEnemy[0]}!')
+            erase(playerPos)
+            time.sleep(2)
             try:
-                inventory.append()
+                inventory.append(tempEnemy[3])
+                return 'survived'
             except:
                 return 'survived'
         
@@ -366,18 +403,55 @@ def combat(enemy): #The combat system for enemies. Takes a specific enemy, lets 
             return 'dead'
 
 def zone():
-    if playerPos in area(1, 1, 64, 64):
+    if playerPos in area(1, 1, 20, 20):
         return "Rolling Hills"
-    elif playerPos in area(33, 65, 96, 160) or playerPos in area(65, 33, 128, 96):
+    elif playerPos in area(11, 21, 30, 50) or playerPos in area(21, 11, 40, 80):
         return "Grassy Plains"
+    elif playerPos in area(31, 31, 60, 60):
+        return "Dark Forest"
+    elif playerPos in area(1, 41, 20, 80) or playerPos in area(-9, 61, 10, 90):
+        return "Dune Desert"
+    elif playerPos in area(-19, 41, 0, 80):
+        return "The Ocean"
+    elif playerPos in area(11, 71, 40, 100):
+        return "Windy Cliffs"
+    elif playerPos in area(61, 51, 70, 80):
+        return "Rapids"
+    elif playerPos in area(41, 61, 60, 80):
+        return "Bog Swamp"
+    elif playerPos in area(21, 101, 30, 120):
+        return "Mt. Doom"
+    else:
+        return "Nowhere"
+    
 def zoneEnemies():
-    if zone() == "Rolling Hills:":
-        return False
-    elif zone() == "Grassy Plains":
-        return [0, 1]
-
+    if "Rolling Hills:" in zone():
+        return ""
+    elif "Grassy Plains" in zone():
+        return random.randint(0, 1)
+    elif "Ocean" in zone():
+        return random.randint(2, 3)
+    elif "Dark Forest" in zone():
+        return random.randint(4, 5)
+    elif "Dune Desert" in zone():
+        return random.randint(6, 7)
+    elif "Rapids" in zone():
+        return random.randint(8, 9)
+    elif "Bog Swamp" in zone():
+        return random.randint(10, 11)
+    elif "Windy Cliffs" in zone():
+        return random.randint(12, 13)
+    elif "Mt. Doom" in zone():
+        return random.randint(14, 15)
+    
+prevZone = copy.copy(zone())
 while moveError: #The loop that starts running the game. Includes a method of restarting the game on death.
-    print(move())
+    if prevZone != zone():
+        print()
+        print(f"Entering new zone! Welcome to: {zone()}!")
+        prevZone = copy.copy(zone())
+        time.sleep(2)
+    move()
     playerPos = move()
     playMap()
     if 'enemy' in collision():
@@ -388,10 +462,27 @@ while moveError: #The loop that starts running the game. Includes a method of re
                     moveError = False
                     break
                 elif restartGame == 'y':
-                    playerPos = [0, 0]
+                    playerPos = [1, 1]
                     health = 100
                     inventory = []
-                    zone = "Rolling Hills"
+                    prevZone = "Rolling Hills"
+                    break
+                else:
+                    print('Invalid input. Try again.')
+                    restartGame = input('Game over. Play again? Y/n: ').lower()
+    elif random.randint(1, 100) == 1 and zone() not in ["Nowhere", "Rolling Hills"]:
+        if 'dead' in combat(zoneEnemies()):
+            restartGame = input('Game over. Play again? Y/n: ').lower()
+            while True:
+                if restartGame == 'n':
+                    moveError = False
+                    break
+                elif restartGame == 'y':
+                    playerPos = [1, 1]
+                    health = 100
+                    inventory = []
+                    coins = 0
+                    prevZone = "Rolling Hills"
                     break
                 else:
                     print('Invalid input. Try again.')
@@ -403,10 +494,10 @@ while moveError: #The loop that starts running the game. Includes a method of re
                 moveError = False
                 break
             elif restartGame == 'y':
-                playerPos = [0, 0]
+                playerPos = [1, 1]
                 health = 100
                 inventory = []
-                zone = "Rolling Hills"
+                prevZone = "Rolling Hills"
                 break
             else:
                 print('Invalid input. Try again.')
